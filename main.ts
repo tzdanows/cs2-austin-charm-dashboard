aa721932b61615bc1f51e822ab9f53ad1f63a384
Tom Zdanowski <tomzdanows@gmail.com>
2025-07-17 19:37:39 -0500

    feat(deploy): migrate from GitHub Pages to Deno Deploy

--- main.ts
+++ main.ts
@@ -5,20 +5,12 @@ export const app = new App<State>();
 
 app.use(staticFiles());
 
-// this is the same as the /api/:name route defined via a file. feel free to delete this!
-app.get("/api2/:name", (ctx) => {
-  const name = ctx.params.name;
-  return new Response(
-    `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
-  );
-});
-
-// this can also be defined via a file. feel free to delete this!
-const exampleLoggerMiddleware = define.middleware((ctx) => {
+// Middleware for request logging
+const loggerMiddleware = define.middleware((ctx) => {
   console.log(`${ctx.req.method} ${ctx.req.url}`);
   return ctx.next();
 });
-app.use(exampleLoggerMiddleware);
+app.use(loggerMiddleware);
 
 await fsRoutes(app, {
   loadIsland: (path) => import(`./islands/${path}`),
